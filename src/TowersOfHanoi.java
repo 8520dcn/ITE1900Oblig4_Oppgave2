fdf6b71ff648e1f42dc297c09967cfe1ad581e81
Dag Christian Nygaard <dag_christian_nygaard@hotmail.com>
2016-04-24 18:00:28 +0200

    Versjon 0.3

--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class TowersOfHanoi {
 
-	public static Integer antFlytninger = 0;
+	private static Integer antFlytninger = 0;
+	private static Integer antMetodeKall = 0;
 
 	public void solve(int n, String start, String auxiliary, String end) {
 		leggTilFlytning();
 		if (n == 1) {
 			System.out.println(start + " -> " + end);
 		} else {
+			leggTilKall();
 			solve(n - 1, start, end, auxiliary);
 			System.out.println(start + " -> " + end);
 			solve(n - 1, auxiliary, start, end);
@@ -24,9 +26,14 @@ public class TowersOfHanoi {
 		int discs = scanner.nextInt();
 		towersOfHanoi.solve(discs, "A", "B", "C");
 		System.out.println("Antall flytninger: " + antFlytninger);
+		System.out.println("Antall rekursive kall: " + antMetodeKall);
 	}
 
 	private void leggTilFlytning() {
 		antFlytninger++;
+	}
+	
+	private void leggTilKall() {
+		antMetodeKall++;
 	}
 }
