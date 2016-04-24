ed39cdfacc84c248b0442dcfdf88c7bb5d84bfad
Dag Christian Nygaard <dag_christian_nygaard@hotmail.com>
2016-04-24 17:52:12 +0200

    Versjon 0.2

--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -1,23 +1,32 @@
+
 //Kilde: http://www.javawithus.com/programs/towers-of-hanoi
 import java.util.Scanner;
 
 public class TowersOfHanoi {
 
-   public void solve(int n, String start, String auxiliary, String end) {
-       if (n == 1) {
-           System.out.println(start + " -> " + end);
-       } else {
-           solve(n - 1, start, end, auxiliary);
-           System.out.println(start + " -> " + end);
-           solve(n - 1, auxiliary, start, end);
-       }
-   }
+	public static Integer antFlytninger = 0;
 
-   public static void main(String[] args) {
-       TowersOfHanoi towersOfHanoi = new TowersOfHanoi();
-       System.out.print("Skriv inn antall disker i tarnet: ");
-       Scanner scanner = new Scanner(System.in);
-       int discs = scanner.nextInt();
-       towersOfHanoi.solve(discs, "A", "B", "C");
-   }
+	public void solve(int n, String start, String auxiliary, String end) {
+		leggTilFlytning();
+		if (n == 1) {
+			System.out.println(start + " -> " + end);
+		} else {
+			solve(n - 1, start, end, auxiliary);
+			System.out.println(start + " -> " + end);
+			solve(n - 1, auxiliary, start, end);
+		}
+	}
+
+	public static void main(String[] args) {
+		TowersOfHanoi towersOfHanoi = new TowersOfHanoi();
+		System.out.print("Skriv inn antall disker i tarnet: ");
+		Scanner scanner = new Scanner(System.in);
+		int discs = scanner.nextInt();
+		towersOfHanoi.solve(discs, "A", "B", "C");
+		System.out.println("Antall flytninger: " + antFlytninger);
+	}
+
+	private void leggTilFlytning() {
+		antFlytninger++;
+	}
 }
